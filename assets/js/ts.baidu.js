68e578a81270e4e0f7f6897bc482a084f533f2e5
zhanglonglong <zhanglonglong@xianlaohu.com>
2015-10-19 14:39:26 +0800

    添加访问量统计

---
+++ assets/js/ts.baidu.js
@@ -0,0 +1,12 @@
+﻿/*
+Baidu traffic statistics
+Site: http://sitecenter.baidu.com/
+Date: 2015-10-19
+*/
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?953cca6644e2b9b7ea90e9ee96dc6c11";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
